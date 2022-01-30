5670d8018b817f93e188be518241528f1798bd21
LAPTOP-0ITQU6AK\lenad <lena.doroshenko.97@mail.ru>
2022-01-30 20:33:06 +0300

    Les16: Answ3. Adding a new data source 'customer_crm'

--- models/report/rep_customer_history.sql
+++ models/report/rep_customer_history.sql
@@ -5,21 +5,37 @@
     )
 }}
 
-with pit_cust as(
-    select hc.customer_pk
+
+
+select hc.customer_key::integer
+    ,hc.customer_pk
+    ,sc.first_name
+    ,sc.last_name
+    ,sc.email
+    ,scc.country
+    ,scc.age
+    ,'{{ var('given_dttm') }}'::timestamp(0) as given_dttm
+from {{ ref('hub_customer')}} as hc 
+left join (
+    select sc.customer_pk
         ,sc.first_name
         ,sc.last_name
         ,sc.email
-        ,sc.effective_from
+        ,sc.effective_from 
         ,coalesce(lead(sc.effective_from) over(partition by sc.customer_pk order by sc.effective_from)-interval '1 second'
             ,'5999-12-31 00:00:00'::timestamp) as effective_to
-from {{ ref('hub_customer')}} as hc 
-    left join {{ref('sat_customer_details')}} as sc 
+    from {{ref('sat_customer_details')}} as sc 
+    )   as sc
 on hc.customer_pk=sc.customer_pk
-)
-
-select t.* 
-    ,'{{ var('given_dttm') }}'::timestamp(0) as given_dttm
-from pit_cust t
-where 1=1
-    and '{{ var('given_dttm') }}'::timestamp between effective_from and effective_to
+    and '{{ var('given_dttm') }}'::timestamp between sc.effective_from and sc.effective_to
+left join (    
+    select scc.customer_pk
+        ,scc.country
+        ,scc.age
+        ,scc.effective_from 
+        ,coalesce(lead(scc.effective_from) over(partition by scc.customer_pk order by scc.effective_from)-interval '1 second'
+            ,'5999-12-31 00:00:00'::timestamp) as effective_to
+    from  {{ref('sat_customer_crm_details')}} as scc
+    ) as scc
+on hc.customer_pk=scc.customer_pk
+    and '{{ var('given_dttm') }}'::timestamp between scc.effective_from and scc.effective_to
